1e8f01881298a453b10dc492d172260f6c429a3b
Stiven <113300330+JStiiven@users.noreply.github.com>
2023-11-08 11:12:58 -0500

    Update index.js

--- 2/index.js
+++ 2/index.js
@@ -6,13 +6,11 @@ while (contador <= 10) {
     if(usuario == numeroSecreto){
         console.log("¡Felicidades adivinaste el numero!");
         break;
-    }
-    else if (numeroSecreto > usuario){
+    }else if (numeroSecreto > usuario){
         console.log("El numero secreto es mayor a:" , usuario , "intenta de nuevo");
     }else if (numeroSecreto < usuario){
         console.log("El numero secreto es menor a:" , usuario , "intenta de nuevo");
-    }
-    if(contador == 11){
+    }if(contador == 11){
         console.log(" Lo siento, no has adivinado el número secreto \n" , "El numero secreo era: " + numeroSecreto);
         break;
     }
